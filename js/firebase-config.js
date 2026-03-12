e3b6c5f59cd44e6f4923a9f13b0ceef55423e0f3
arpitmishr <74408351+arpitmishr@users.noreply.github.com>
2026-03-12 09:30:37 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
+// TODO: Replace with your project's customized config object from Firebase console
+const firebaseConfig = {
+  apiKey: "AIzaSyBtY97ItVEcZ7srTcpIDUsXmqf1ZBlW2ZQ",
+  authDomain: "store-manager-87634.firebaseapp.com",
+  projectId: "store-manager-87634",
+  storageBucket: "store-manager-87634.firebasestorage.app",
+  messagingSenderId: "620672866976",
+  appId: "1:620672866976:web:1ae1157027a2a0705f47c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Auth and Database to use in other files
+export const auth = getAuth(app);
+export const db = getFirestore(app);
