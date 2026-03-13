f93875cb207998ae6b170b243591ea28f5d8c3f6
arpitmishr <74408351+arpitmishr@users.noreply.github.com>
2026-03-13 05:50:50 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+import { setupAuth } from './auth.js';
+import { addItem, listenToInventory } from './inventory.js';
+import { processSale } from './sales.js';
 
+// Holds our stock data so we can update UI anywhere
+let globalInventory =
