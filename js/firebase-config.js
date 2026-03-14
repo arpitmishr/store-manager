0300d75d6185c983e27e4c5d940b45adc1ad8d1c
arpitmishr <74408351+arpitmishr@users.noreply.github.com>
2026-03-14 17:39:21 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,7 @@
-
+// js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyBtY97ItVEcZ7srTcpIDUsXmqf1ZBlW2ZQ",
@@ -13,9 +12,6 @@ const firebaseConfig = {
   appId: "1:620672866976:web:1ae1157027a2a0705f47c5"
 };
 
-
 const app = initializeApp(firebaseConfig);
-
-
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
